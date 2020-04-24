6ef173ce5bb8b74ffa55d4f572756f07402a0708
Athiwat Hirunworawongkun <athivvat@gmail.com>
2020-04-24 20:59:39 +0700

    Inital

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	let bio = false
+</script>
+
+<main>
+	<h1>> Hello, world!_</h1>
+	<h2>Athiwat Hirunworawongkun</h2>
+	<div class="bio">
+		<p>Founder of Humaan.ai—Build intelligent technology tools that amplifying human ingenuity.</p>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-family: 'Roboto Mono', monospace;
+		color: #000;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.bio {
+		margin: 0 auto;
+		text-align: left;
+		text-align: center;
+		max-width: 400px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
