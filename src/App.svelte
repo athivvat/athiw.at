b27d0c4f685fbece01cab96f1e1d1255a7392db5
Athiwat Hirunworawongkun <athivvat@gmail.com>
2020-05-05 21:49:09 +0700

    Updated information

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,71 @@
-<script>
-	let bio = false
-</script>
-
 <main>
-	<h1>> Hello, world!_</h1>
-	<h2>Athiwat Hirunworawongkun</h2>
-	<div class="bio">
-		<p>Founder of Humaan.ai—Build intelligent technology tools that amplifying human ingenuity.</p>
+	<div class="hero">
+		<h1>> Humanity Needs an Upgrade._</h1>
+		<h2>Athiwat Hirunworawongkun</h2>
+		<div class="bio">
+			<p>Founder of Humaan.ai—Build intelligent technology tools that amplifying human ingenuity.</p>
+		</div>
+	</div>
+	<div class="work-experience">
+		<h3>Work Experience</h3>
+		<div class="work-experience-container">
+			<div class="item">
+				<p>2020, 1 April - Present</p>
+				<p><strong>Founder</strong></p>
+				<p><a href="https://humaan.ai">Humaan AI</a></p>
+			</div>
+			<div class="item">
+				<p>2020, 1 February - Present</p>
+				<p><strong>Data Engineer</strong></p>
+				<p><a href="https://kingpowerclick.com/">King Power Click</a></p>
+			</div>
+			<div class="item">
+				<p>2017, 1 May - 2020, January 31</p>
+				<p><strong>Software Engineer</strong></p>
+				<p><a href="https://kingpowerclick.com/">King Power Click</a></p>
+			</div>
+		</div>
+	</div>
+	<div class="education">
+		<h3>Eduction</h3>
+		<div class="education-container">
+			<div class="item">
+				<p>2019-Present</p>
+				<p><strong>M.S. Business Analytics and Data Science</strong></p>
+				<p>Major in Artificial Intelligence and Machine Learning</p>
+				<p><a href="http://www.nida.ac.th/en/">National Institute of Development Administration (NIDA)</a></p>
+			</div>
+			<div class="item">
+				<p>2008-2011</p>
+				<p><strong>B.Eng. Computer Engineering</strong></p>
+				<p><a href="https://www.tni.ac.th/">Thai-Nichi Institute of Technology</a></p>
+			</div>
+		</div>
 	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	.hero {
+		text-align: center;
+		border-bottom: 1px solid #ccc;
+		padding-bottom: 35px;
+		margin-bottom: 50px;
+	}
 	h1 {
 		font-family: 'Roboto Mono', monospace;
 		color: #000;
-		font-size: 4em;
+		font-size: 2.2em;
 		font-weight: 100;
+	}
+
+	h3 {
+		background: #f7f8fa;
+		padding: 5px 10px;
 	}
 
 	.bio {
@@ -32,9 +75,20 @@
 		max-width: 400px;
 	}
 
+	.education p, .work-experience p {
+		margin: 5px 0;
+	}
+
+	.education .item, .work-experience .item {
+		margin-bottom: 20px;
+	}
+	.education-container, .work-experience-container {
+		padding: 10px;
+	}
+
 	@media (min-width: 640px) {
 		main {
-			max-width: none;
+			max-width: 680px;
 		}
 	}
 </style>
