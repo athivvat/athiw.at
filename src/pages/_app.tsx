5eaa622d84d5a06c3bead389f4e728bcc52ca58a
athivvat <athivvat@gmail.com>
2020-12-08 13:37:40 +0700

    Add NextSEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { Layout } from '../components';
+import { NextSeo } from 'next-seo';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextSeo
+        title="Athiwat Hirunworawongkun"
+        description="Humanity Needs an Upgrade."
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
