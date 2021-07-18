9ce400a68579a15c32420035870ef77a73d5313d
athivvat <athivvat@gmail.com>
2021-07-18 20:27:02 +0700

    Clean

--- src/components/Certificates/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Certificates } from './Certificates'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import styled from 'styled-components';
-import { Certificates } from '../components/certificates';
-import { Education } from '../components/education';
-import { Experience } from '../components/experience';
-import { Footer } from '../components/footer';
 
 const Content = styled.div`
   display: grid;
@@ -10,38 +6,142 @@ const Content = styled.div`
   column-gap: 100px;
 `;
 
+const Container = styled.div`
+  margin-bottom: 25px;
+`;
+
+const JobTitle = styled.p`
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
+`;
+
+const Company = styled.a`
+  margin: 0;
+  padding: 0;
+`;
+
+const Period = styled.p`
+  margin: 0;
+  padding: 0;
+  color: #757575;
+  font-size: 14px;
+`;
+
+const Degree = styled.p`
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
+`;
+
+const Major = styled.p`
+  margin: 0;
+  padding: 0;
+`;
+
+const University = styled.a``;
+
+const Items = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+// const Item = styled.li`
+//   &:before {
+//     content: '\\29BF';
+//     color: #fff;
+//     padding-right: 8px;
+//   }
+// `;
 
 function HomePage() {
   return (
     <>
       <div>
-      <Content>
-        <div>
-          <h3>About</h3>
-          <p>
-            Founder of Humaan.ai—Build intelligent technology tools that
-            amplifying human ingenuity.
-          </p>
-          <p>
-            I'm a Data Engineer who is really passionate to become entrepreneur,
-            investor, and AI engineer. Was found Humaan.ai to bringing the AI to
-            everyone to do incredible ways.
-          </p>
-          <p>
-            Currently, I'm studying for a master's degree in Business Analytics
-            and Data Sciences, where majoring in Artificial Intelligence and
-            Machine Learning at National Institute of Development Administration
-            (NIDA), Thailand.
-          </p>
-        </div>
-        <div>
-          <Experience />
-          <Education />
-          <Certificates />
-        </div>
-      </Content>
-    </div>
-      <Footer />
+        <Content>
+          <div>
+            <h3>About</h3>
+            <p>
+              Founder of Humaan.ai—Build intelligent technology tools that
+              amplifying human ingenuity.
+            </p>
+            <p>
+              I'm a Data Engineer who is really passionate to become
+              entrepreneur, investor, and AI engineer. Was found Humaan.ai to
+              bringing the AI to everyone to do incredible ways.
+            </p>
+            <p>
+              Currently, I'm studying for a master's degree in Business
+              Analytics and Data Sciences, where majoring in Artificial
+              Intelligence and Machine Learning at National Institute of
+              Development Administration (NIDA), Thailand.
+            </p>
+          </div>
+          <div>
+            <h3>Experience</h3>
+            <Container>
+              <JobTitle>Data Engineer</JobTitle>
+              <Company
+                href="http://kingpowerclick.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                King Power Click
+              </Company>
+              <Period>January 2020 - Present</Period>
+            </Container>
+            <Container>
+              <JobTitle>Software Engineer</JobTitle>
+              <Company
+                href="http://kingpowerclick.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                King Power Click
+              </Company>
+              <Period>May 2018 - January 2020</Period>
+            </Container>
+            <h3>Education</h3>
+            <div>
+              <Degree>M.S. Business Analytics and Data Science</Degree>
+              <Major>
+                Major in Artificial Intelligence and Machine Learning
+              </Major>
+              <University
+                href="http://www.nida.ac.th/en/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                National Institute of Development Administration (NIDA)
+              </University>
+              <Period>2020-Present</Period>
+            </div>
+            <div>
+              <Degree>B.Eng. Computer Engineering</Degree>
+              <University
+                href="https://www.tni.ac.th/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Thai-Nichi Institute of Technology
+              </University>
+              <Period>2008-2011</Period>
+            </div>
+            <h3>Certificates</h3>
+            <div>
+              <a
+                href="https://verify.openedg.org/?id=rDPA.xKsx.fFK4"
+                target="_blank"
+                rel="noreferrer"
+              >
+                PCAP – Certified Associate in Python Programming
+              </a>
+              <div>Date Certified: July 28, 2020</div>
+              <div>Certification Code: rDPA.xKsx.fFK4</div>
+            </div>
+          </div>
+        </Content>
+      </div>
     </>
   );
 }
